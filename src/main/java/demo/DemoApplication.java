c4a3e605d20e39d0b7ea98f4796848dc167b9d75
조준영 <whwnsdud2024@naver.com>
2025-06-15 22:56:52 +0900

    2025.06.15 <조준영> 패키지 스캔 범위 수정

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"demo", "core"}) // "demo"는 기존 스캔 범위, "core"는 추가할 패키지
 public class DemoApplication {
 
 	public static void main(String[] args) {
